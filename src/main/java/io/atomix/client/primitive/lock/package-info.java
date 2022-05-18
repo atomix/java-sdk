a26cce2b911a72e9b9be603375245d411c175a35
pierventre <pier.ventre@intel.com>
2022-05-18 17:35:02 +0200

    [SDFAB-1209] Re-sync atomix-java-client with the new runtime/api
    Proto APIs are now hosted under atomix/runtime project,
    reconcile also with the new structure. Additionally,
    updates tooling to be compatible with Apple silicons.

--- src/main/java/io/atomix/client/primitive/lock/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2022-present Open Networking Foundation
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Lock abstraction.
- */
-package io.atomix.client.primitive.lock;
--- src/main/java/io/atomix/client/primitive/map/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2022-present Open Networking Foundation
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Map abstraction.
- */
-package io.atomix.client.primitive.map;
