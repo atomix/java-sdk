57d09b8ffc675857c652ed755f9513f3c5965a2f
Jordan Halterman <jordan.halterman@intel.com>
2022-08-16 12:14:56 -0700

    Add copyright headers to all Java files

--- src/main/java/io/atomix/client/iterator/AsyncIterator.java
+++ src/main/java/io/atomix/client/iterator/AsyncIterator.java
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2018-present Open Networking Foundation
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package io.atomix.client.iterator;
 
 import io.atomix.client.DistributedPrimitive;
